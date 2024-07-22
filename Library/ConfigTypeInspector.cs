d945274a8db79d0d9b87663c7d0bf8493b003d08
Sean Fausett <sean@jellyfish.co.nz>
2024-07-22 14:02:38 +1200

    Fix config type inspector

--- Library/ConfigTypeInspector.cs
+++ Library/ConfigTypeInspector.cs
@@ -7,6 +7,10 @@ public sealed class ConfigTypeInspector : TypeInspectorSkeleton
         Inner = inner;
     }
 
+    public override string GetEnumName(Type enumType, string name) => Inner.GetEnumName(enumType, name);
+
+    public override string GetEnumValue(object enumValue) => Inner.GetEnumValue(enumValue);
+
     public override IEnumerable<IPropertyDescriptor> GetProperties(Type type, object? container) =>
         Inner.GetProperties(type, container).Where(property => !Regex.IsMatch(property.Name, @"Password|Secret|Token")); // ignore secrets
 
